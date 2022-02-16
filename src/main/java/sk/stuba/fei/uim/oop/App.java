a101e62e74e16387f0fc18d738103db239298aa9
TomasKovyKovacik <tomas_kovacik@stuba.sk>
2022-02-16 16:06:59 +0100

    premenne

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -2,6 +2,21 @@ package sk.stuba.fei.uim.oop;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int celeCislo = 5;
+        long celeVacsieCislo = 5L;
+        float desatinneCislo = 5.5f;
+        double dlhsieDesatinneCislo = 5.5;
+        char znak = 'a';
+        String retazec = "retazec";
+        boolean bool = false;
+
+        int podiel = 2/3; // 0
+        System.out.println(podiel);
+
+        int zvysok = 2%3; // 2
+        System.out.println(zvysok);
+
+        float podielFloat = 2/3.0f;
+        System.out.println(podielFloat);
     }
 }
