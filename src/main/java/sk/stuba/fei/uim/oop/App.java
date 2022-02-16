9dfb63315ce9c9c0e76fc955ba6a789ad20a901d
TomasKovyKovacik <tomas_kovacik@stuba.sk>
2022-02-16 16:21:32 +0100

    funkcia, pole, for

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -2,21 +2,18 @@ package sk.stuba.fei.uim.oop;
 
 public class App {
     public static void main(String[] args) {
-        int celeCislo = 5;
-        long celeVacsieCislo = 5L;
-        float desatinneCislo = 5.5f;
-        double dlhsieDesatinneCislo = 5.5;
-        char znak = 'a';
-        String retazec = "retazec";
-        boolean bool = false;
+        int[] pole = new int[10];
 
-        int podiel = 2/3; // 0
-        System.out.println(podiel);
+        for (int i = 0; i < pole.length; i++) {
+            pole[i] = i;
+        }
 
-        int zvysok = 2%3; // 2
-        System.out.println(zvysok);
+        for (int i = 0; i < pole.length; i++) {
+            App.vypis(i);
+        }
+    }
 
-        float podielFloat = 2/3.0f;
-        System.out.println(podielFloat);
+    public static void vypis(int a) {
+        System.out.println(a);
     }
 }
